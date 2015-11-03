02d90010615a3b14079c61b0a7a79b407361ca81
mmahnken <mmm25eg@gmail.com>
2015-11-03 11:26:18 -0800

    initial commit

---
+++ colors.sh
@@ -0,0 +1,88 @@
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+  # We have color support; assume it's compliant with Ecma-48
+  # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+  # a case would tend to support setf rather than setaf.)
+  color_prompt=yes
+    else
+  color_prompt=yes
+    fi
+fi
+
+C_WHITE='\[\e[1;37m\]'
+C_LIGHTGRAY='\[\e[0;37m\]'
+C_GRAY='\[\e[1;30m\]'
+C_BLACK='\[\e[0;30m\]'
+C_RED='\[\e[0;31m\]'
+C_LIGHTRED='\[\e[1;31m\]'
+C_GREEN='\[\e[0;32m\]'
+C_LIGHTGREEN='\[\e[1;92m\]'
+C_BROWN='\[\e[0;33m\]'
+C_YELLOW='\[\e[1;33m\]'
+C_BLUE='\[\e[0;34m\]'
+C_LIGHTBLUE='\[\e[1;34m\]'
+C_PURPLE='\[\e[0;35m\]'
+C_PINK='\[\e[1;35m\]'
+C_CYAN='\[\e[0;36m\]'
+C_LIGHTCYAN='\[\e[1;36m\]'
+C_DEFAULT='\[\e[0m\]'
+
+function setsymbols() {
+  local exit_status=$?
+  local color_exit=
+  local color_exit2=
+  local color_exit3=
+  local usym=
+  local end_sym=\$
+  local usegit=1
+  local id=`/usr/bin/id -u`
+  local currenthour=$(date +%-H)
+  [[ $id == 0 ]] && end_sym=\#
+  if [ $exit_status = 0 ]; then
+    if [[ $id == 0 ]]; then
+      color_exit="$C_LIGHTBLUE"; color_exit2="$C_BLUE"; color_exit3="$C_WHITE";
+    else
+      color_exit="$C_LIGHTBLUE"; color_exit2="$C_DEFAULT"; color_exit3="$C_LIGHTBLUE";
+    fi
+    usym=":"
+  else
+    if [[ $id == 0 ]]; then
+      color_exit="$C_LIGHTRED"; color_exit2="$C_RED"; color_exit3="$C_BROWN"; exit_emoji=💩;
+    else
+      color_exit="$C_LIGHTRED"; color_exit2="$C_RED"; color_exit3="$C_LIGHTCYAN";exit_emoji=💩;
+    fi
+    usym="!"
+  fi
+  if (("$currenthour" > 18)); then
+    exit_emoji=🌙;
+  else
+    exit_emoji=🌵;
+  fi
+  PS1="${VIRTUAL_ENV:+$C_PURPLE($(basename $VIRTUAL_ENV)) }${C_DEFAULT}${exit_emoji}  \
+${color_exit}\u\
+${color_git}${usym}\
+${color_exit3}\w${color_exit2}${color_job}\
+${end_sym}\
+${C_DEFAULT} "
+}
+
+if [ "$color_prompt" = yes ]; then
+    PROMPT_COMMAND=setsymbols
+else
+    PS1='\u@${HOSTNAME}:\w\$ '
+fi
+unset color_prompt force_color_prompt
+# ️🔷😈💛💙
+# # ⚡️🔴🔱💥🌟👍👎💔💜💚
+# 💩
+# 😎
---
+++ profile.sh
@@ -0,0 +1,42 @@
+#Setting PATH for EPD_free-7.3-2
+# The orginal version is saved in .bash_profile.pysave
+PATH="/usr/local/bin/git:$PATH"
+PATH="/usr/local/bin/pandoc:$PATH"
+
+PATH="/usr/local/bin:$PATH"
+PATH="/Library/Frameworks/Python.framework/Versions/Current/bin:$PATH"
+PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
+#PATH="/usr/local/var/postgres9.3:$PATH"
+export CLICOLOR=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+source ~/bash_colors/Settings_files/color-prompt.sh
+alias path='echo $PATH | tr -s ":" "\n"'
+
+### Added by the Heroku Toolbelt
+PATH=/usr/local/bin:"/usr/local/heroku/bin:$PATH"
+PATH="/usr/local/bin/scala/bin:$PATH"
+PATH="/usr/local/bin/java:$PATH"
+
+
+PATH="$PATH:~/bin"
+`launchctl setenv PATH $PATH`
+
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
+eval "$(rbenv init -)"
+##
+# Your previous /Users/meggiemahnken/.bash_profile file was backed up as /Users/meggiemahnken/.bash_profile.macports-saved_2014-11-05_at_11:35:35
+##
+
+# MacPorts Installer addition on 2014-11-05_at_11:35:35: adding an appropriate PATH variable for use with MacPorts.
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+# Finished adapting your PATH environment variable for use with MacPorts.
+export PIAS_OPT_TERMINAL="xterm-256color"
+
+
+export PS1="___________________    | \w @ \h (\u) \n| => "
+export PS2="| => "
+export PGHOST=localhost
+
